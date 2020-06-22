ea88c6b15a20c5ac1b6b8e97f1c9325051b25c60
artemynouveau <artnovkov@gmail.com>
2020-06-22 14:40:24 +0300

    contacts actions redux

---
+++ src/store/reducers/contacts/contacts.js
@@ -0,0 +1,24 @@
+import * as actionTypes from '../../actions/contacts/actionTypes'
+import {updateObject} from "../utility";
+
+const initialState = {
+    contacts: [],
+    loading: false,
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_CONTACT_SUCCESS:
+            return updateObject(state, {loading: false, contacts: action.contacts})
+        case actionTypes.FETCH_CONTACTS_FAIL:
+            return updateObject(state, {loading: false})
+        case actionTypes.FETCH_CONTACTS_START:
+            return updateObject(state, {loading: true})
+        case actionTypes.ADD_CONTACT_SUCCESS:
+            return updateObject(state, {contacts: state.contacts.concat(action.contact)})
+        default:
+            return state
+    }
+};
+
+export default reducer
