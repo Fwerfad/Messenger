f8444502bcafdb33d833b0e078cf73440b16d715
Fwerf <afs1424ad@gmail.com>
2020-06-26 16:20:56 +0300

    Several fixed on header component: -renamed it to header.js from header-layout.js -moved styles to classes.js -Moved editing ExpansionPanel classes in classes.js for better code organization -created new component in classes.js called HeaderLink, which simplifies the code. -Imported header to App.js

--- src/Component/header.js
+++ src/Component/header.js
@@ -6,11 +6,8 @@ import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import {ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, useStyles, HeaderLink} from './classes';
 import Typography from '@material-ui/core/Typography';
-import useStyles from './classes'
 
 //* Кнопка с переключаемым состоянием, у нас нету /lab, задел на будущее.
 //* import { ToggleButton } from '@material-ui/lab';
@@ -20,81 +17,21 @@ const Header = (props) => {
         e.preventDefault();
         console.log("По ссылке кликнули.");
     }
-    const classes = useStyles;
+    const classes = useStyles();
     console.log("classes");
     console.log(classes);
     return (
         <body>
         <ExpansionPanel>
-            <ExpansionPanelSummary
-                //expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-                style={classes.expansionPanel}
-            >
+            <ExpansionPanelSummary>
                 <Typography>
                     MESSAGES <ExpandMoreIcon/>
                 </Typography>
             </ExpansionPanelSummary>
-            <ExpansionPanelDetails style={classes.expansionPanelContent}>
-                <Box boxShadow={3} style={classes.box}>
-                    <div style={{
-                        width: "100%",
-                        height: "80px",
-                        position: "relative",
-                        color: "darkgrey",
-                        background: "whitesmoke",
-                    }}>
-                        <button onClick={handleClick} style={{
-                            display: "inline-block",
-                            height: "80px",
-                            width: "100px",
-                            border: "0px",
-                            background: "whitesmoke"
-                        }}>
-                            <Grid container
-                                  spacing={0}
-                                  direction="column"
-                                  alignItems="center"
-                                  justify="center">
-                                <Grid item xs><ChatIcon/></Grid>
-                                <Grid item xs style={{font: "20px"}}>Chat</Grid>
-                            </Grid>
-                        </button>
-                        <button onClick={handleClick} style={{
-                            display: "inline-block",
-                            height: "80px",
-                            width: "100px",
-                            border: "0px",
-                            background: "whitesmoke"
-                        }}>
-                            <Grid container
-                                  spacing={0}
-                                  direction="column"
-                                  alignItems="center"
-                                  justify="center">
-                                <Grid item xs><AccountCircleIcon/></Grid>
-                                <Grid item xs style={{font: "20px"}}>Profile</Grid>
-                            </Grid>
-                        </button>
-                        <button onClick={handleClick} style={{
-                            display: "inline-block",
-                            height: "80px",
-                            width: "100px",
-                            border: "0px",
-                            background: "whitesmoke"
-                        }}>
-                            <Grid container
-                                  spacing={0}
-                                  direction="column"
-                                  alignItems="center"
-                                  justify="center">
-                                <Grid item xs><PeopleAltIcon/></Grid>
-                                <Grid item xs style={{font: "20px"}}>Contacts</Grid>
-                            </Grid>
-                        </button>
-                    </div>
-                </Box>
+            <ExpansionPanelDetails>
+                <HeaderLink fun={handleClick} text={'Chat'} buttonClass={classes.button} img={<ChatIcon/>}/>
+                <HeaderLink fun={handleClick} text={'Profile'} buttonClass={classes.button} img={<AccountCircleIcon/>}/>
+                <HeaderLink fun={handleClick} text={'Contacts'} buttonClass={classes.button} img={<PeopleAltIcon/>}/>
             </ExpansionPanelDetails>
         </ExpansionPanel>
         </body>
