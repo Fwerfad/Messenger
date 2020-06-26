301b09c46843145ebb193de8fa662b8515e8a7e6
Fwerf <afs1424ad@gmail.com>
2020-06-26 16:22:44 +0300

    deleted unused imports

--- src/Component/header.js
+++ src/Component/header.js
@@ -1,22 +1,17 @@
 import React from "react";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ChatIcon from "@material-ui/icons/Chat";
-import RateReviewIcon from "@material-ui/icons/RateReview";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import {ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, useStyles, HeaderLink} from './classes';
 import Typography from '@material-ui/core/Typography';
-
-//* Кнопка с переключаемым состоянием, у нас нету /lab, задел на будущее.
-//* import { ToggleButton } from '@material-ui/lab';
 
 const Header = (props) => {
     function handleClick(e) {
         e.preventDefault();
         console.log("По ссылке кликнули.");
     }
+
     const classes = useStyles();
     console.log("classes");
     console.log(classes);
