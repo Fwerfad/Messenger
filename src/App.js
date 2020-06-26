d780ccb77c293fd3a28166251d62a0002ed539ec
dimonker <rebrik.dmitry@gmail.com>
2020-06-26 20:25:21 +0300

    fix: extra files deleted move firebaseConfig and uiConfig to separate module

--- src/App.js
+++ src/App.js
@@ -1,49 +1,20 @@
 import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
-import firebase from "firebase"
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
-
-var firebaseConfig = {
-  apiKey: "AIzaSyAIKF3LpD4fnzmcktXOOgNCtMcxD0qBzEY",
-  authDomain: "messenger-3d1ca.firebaseapp.com",
-  databaseURL: "https://messenger-3d1ca.firebaseio.com",
-  projectId: "messenger-3d1ca",
-  storageBucket: "messenger-3d1ca.appspot.com",
-  messagingSenderId: "1097565193877",
-  appId: "1:1097565193877:web:4655dd1413fdb994c87d9c",
-}
-
-firebase.initializeApp(firebaseConfig)
+import { firebase, uiConfig } from "./firebaseConfig"
 
 class App extends React.Component {
-  // The component's Local state.
+  // TODO move isSignedIn to redux
   state = {
-    isSignedIn: false, // Local signed-in state.
+    isSignedIn: false,
   }
 
-  // Configure FirebaseUI.
-  uiConfig = {
-    // Popup signin flow rather than redirect flow.
-    signInFlow: "popup",
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-    ],
-    callbacks: {
-      // Avoid redirects after sign-in.
-      signInSuccessWithAuthResult: () => false,
-    },
-  }
-
-  // Listen to the Firebase Auth state and set the local state.
   componentDidMount() {
     this.unregisterAuthObserver = firebase
       .auth()
       .onAuthStateChanged((user) => this.setState({ isSignedIn: !!user }))
   }
 
-  // Make sure we un-register Firebase observers when the component unmounts.
   componentWillUnmount() {
     this.unregisterAuthObserver()
   }
@@ -54,9 +25,7 @@ class App extends React.Component {
     if (!this.state.isSignedIn) {
       return (
         <div>
-          <h1>My App</h1>
-          <p>Please sign-in:</p>
-          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={auth} />
+          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
         </div>
       )
     }
@@ -67,7 +36,7 @@ class App extends React.Component {
           <div>
             <h1>Messenger</h1>
             <p>Привет {auth.currentUser.displayName}!</p>
-            <a onClick={() => auth.signOut()}>Sign-out</a>
+            <button onClick={() => auth.signOut()}>Sign-out</button>
           </div>
         </header>
       </div>
